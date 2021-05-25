89ac31b12ba3d48d7de4dd05cdef473bd263a8cc
Hồ Trần Tiến <25430491+hotrantien01@users.noreply.github.com>
2021-05-25 10:41:48 +0700

    first commit

---
+++ app/src/main/java/com/example/appdoctruyen/ManThongTinApp.java
@@ -0,0 +1,24 @@
+package com.example.appdoctruyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ManThongTinApp extends AppCompatActivity {
+
+    TextView txtThongTin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_thong_tin_app);
+
+        txtThongTin = findViewById(R.id.textviewthongtin);
+
+        String thongtin = " VKU \n" +
+                " Tien \n" +
+                " Truong\n" +
+                " Vuong ";
+        txtThongTin.setText(thongtin);
+    }
+}
